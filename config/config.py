f09621ecc14069e15f2c997463c712ec54eb84f9
Edwin Henneken <ehenneken@cfa.harvard.edu>
2014-11-01 21:00:17 -0400

    Some cleanup

--- config/config.py
+++ config/config.py
@@ -26,24 +26,8 @@ class AppConfig(object):
     CHUNK_SIZE = 100
     NUMBER_SUGGESTIONS = 10
     SQLALCHEMY_DATABASE_URI = ''
-    METRICS_DEFAULT_MODELS = 'statistics,histograms,metrics,series'
     THRESHOLD_FREQUENCY = 1
     SOLRQUERY_URL = 'http://adswhy:9000/solr/collection1/select'
-    SOLRQUERY_TIMEOUT = 300
-    SOLRQUERY_KEEPALIVE = False
-    SOLRQUERY_HTTP_METHOD = 'POST'
-    SOLRQUERY_EXTRA_PARAMS = [
-        ('hl.maxAnalyzedChars', '150000'),
-        ('hl.requireFieldMatch', 'true'),
-        ('hl.usePhraseHighlighter', 'true'),
-        ('indent', 'true')
-    ]
-
-    SOLR_DOCUMENT_ID_FIELD = 'bibcode'
-    SOLR_FILTER_QUERY_PARSER = 'aqp'
-    SOLR_HIGHLIGHTER_QUERY_PARSER = 'aqp'
-    SOLR_HAPROXY_SESSION_COOKIE_NAME = 'JSESSIONID'
-
     
 try:
     from local_config import LocalConfig
