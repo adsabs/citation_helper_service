6d9752b04864fc5b33e8df6581b761354a2a871f
Edwin Henneken <ehenneken@cfa.harvard.edu>
2014-11-03 21:21:57 -0500

    Implemented blueprint and better exception handling

--- api.py
+++ api.py
@@ -1,32 +1,40 @@
 import sys
 from flask import Flask
 from flask import request
-from flask import jsonify
-from flask.ext.restful import abort, Api, Resource
+from flask import Blueprint
+from flask.ext.restful import Api, Resource
 from config import config
 from citation_helper_utils import get_suggestions
 
-app = Flask(__name__)
-api = Api(app)
+app_blueprint = Blueprint('api', __name__)
+api = Api(app_blueprint)
 
 class CitationHelper(Resource):
-
+    """computes Citation Helper suggestions on the POST body"""
+    scope = 'oauth:suggestions:read'
     def post(self):
         if not request.json or not 'bibcodes' in request.json:
-            abort(400)
+            return {'msg': 'no bibcodes found in POST body'}, 400
         bibcodes = map(lambda a: str(a), request.json['bibcodes'])
 
         try:
             results = get_suggestions(bibcodes=bibcodes)
         except Exception, err:
-            sys.stderr.write('Unable to get results! (%s)' % err)
-            abort(400)
+            return {'msg': 'Unable to get results! (%s)' % err}, 500
 
-        return jsonify(suggestions=results)
+        return results
 
 class Resources(Resource):
+    """Overview of available resources"""
+    scope = 'oauth:resources:read'
     def get(self):
-      return config.RESOURCES
+        func_list = {}
+        for rule in app.url_map.iter_rules():
+            func_list[rule.rule] = {'methods':app.view_functions[rule.endpoint].methods,
+                                    'scope': app.view_functions[rule.endpoint].view_class.scope,
+                                    'description': app.view_functions[rule.endpoint].view_class.__doc__,
+                                       }
+        return func_list
 ##
 ## Actually setup the Api resource routing here
 ##
@@ -34,4 +42,6 @@ api.add_resource(CitationHelper, '/suggestions')
 api.add_resource(Resources, '/resources')
 
 if __name__ == '__main__':
+    app = Flask(__name__, static_folder=None)
+    app.register_blueprint(app_blueprint)
     app.run(debug=True)
