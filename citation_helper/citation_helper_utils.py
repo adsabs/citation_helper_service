ff212416c0b421e8c42f761fbab6a694541a20d2
Edwin Henneken <ehenneken@cfa.harvard.edu>
2014-11-04 19:01:15 -0500

    Refactoring of code

--- citation_helper/citation_helper_utils.py
+++ citation_helper/citation_helper_utils.py
@@ -9,9 +9,7 @@ import sys
 import os
 import operator
 from itertools import groupby
-# BEER-specific imports
-from flask import current_app as app
-from config import config
+from flask import current_app
 from utils import get_references
 from utils import get_citing_papers
 from utils import get_meta_data
@@ -27,9 +25,9 @@ def get_suggestions(**args):
     if len(bibcodes) == 0:
         return []
     # Any overrides for default values?
-    Nsuggestions = config.NUMBER_SUGGESTIONS
+    Nsuggestions = current_app.config['NUMBER_SUGGESTIONS']
     # get rid of potential trailing spaces
-    bibcodes = map(lambda a: a.strip(), bibcodes)[:config.MAX_INPUT]
+    bibcodes = map(lambda a: a.strip(), bibcodes)[:current_app.config['MAX_INPUT']]
     # start processing
     # get the citations for all publications (keeping multiplicity is essential)
     cits = get_citing_papers(bibcodes=bibcodes)
@@ -46,7 +44,7 @@ def get_suggestions(**args):
     # and sort them, most frequent first
     paperFreq = sorted(paperFreq, key=operator.itemgetter(1),reverse=True)
     # remove all papers with frequencies smaller than threshold
-    paperFreq = filter(lambda a: a[1] > config.THRESHOLD_FREQUENCY, paperFreq)
+    paperFreq = filter(lambda a: a[1] > current_app.config['THRESHOLD_FREQUENCY'], paperFreq)
     # get metadata for suggestions
     meta_dict = get_meta_data(results=paperFreq[:Nsuggestions])
     # return results in required format
