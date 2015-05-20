21b0b43431017a974d538207d0af9f28d03eb7e1
Edwin Henneken <ehenneken@cfa.harvard.edu>
2015-05-20 09:57:59 -0400

    made files PEP8-compliant

--- service/utils/utils.py
+++ service/utils/utils.py
@@ -3,41 +3,52 @@ Created on Nov 1, 2014
 
 @author: ehenneken
 '''
-from flask import current_app, request
+from flask import current_app as _app
+from flask import request
 import sys
 import os
 import urllib
 import simplejson as json
+
 
 def get_data(**args):
     """
     Get the references for a set of bibcodes
     """
     references = []
-    citations  = []
+    citations = []
     # This information can be retrieved with one single Solr query
     # (just an 'OR' query of a list of bibcodes)
     # To restrict the size of the query URL, we split the list of
     # bibcodes up in a list of smaller lists
-    biblists = list(chunks(args['bibcodes'], current_app.config.get('CITATION_HELPER_CHUNK_SIZE')))
+    biblists = list(
+        chunks(args['bibcodes'],
+               _app.config.get('CITATION_HELPER_CHUNK_SIZE')))
     for biblist in biblists:
-        q = " OR ".join(map(lambda a: "bibcode:%s"%a, biblist))
+        q = " OR ".join(map(lambda a: "bibcode:%s" % a, biblist))
         # Get the information from Solr
-        # We only need the contents of the 'reference' field (i.e. the list of bibcodes 
-        # referenced by the paper at hand)
-        headers = {'X-Forwarded-Authorization' : request.headers.get('Authorization')}
-        params = {'wt':'json', 'q':q, 'fl':'reference,citation', 'rows': current_app.config['CITATION_HELPER_MAX_HITS']}
-        response = current_app.config.get('CITATION_HELPER_CLIENT').session.get(current_app.config.get('CITATION_HELPER_SOLRQUERY_URL'), params=params)
+        # We only need the contents of the 'reference' field (i.e. the
+        # list of bibcodes referenced by the paper at hand)
+        headers = {'X-Forwarded-Authorization':
+                   request.headers.get('Authorization')}
+        params = {'wt': 'json', 'q': q, 'fl': 'reference,citation',
+                  'rows': _app.config['CITATION_HELPER_MAX_HITS']}
+        response = _app.config.get('CITATION_HELPER_CLIENT').session.get(
+            _app.config.get('CITATION_HELPER_SOLRQUERY_URL'),
+            params=params)
         if response.status_code != 200:
-            return {"Error": "There was a connection error. Please try again later", "Error Info": response.text, "Status Code": response.status_code}
+            return {"Error": "There was a connection error",
+                    "Error Info": response.text,
+                    "Status Code": response.status_code}
         resp = response.json()
         # Collect all bibcodes in a list (do NOT remove multiplicity)
         for doc in resp['response']['docs']:
             if 'reference' in doc:
                 references += doc['reference']
             if 'citation' in doc:
-                citations  += doc['citation']
-    return [bib for bib in references+citations]
+                citations += doc['citation']
+    return [bib for bib in references + citations]
+
 
 def get_meta_data(**args):
     """
@@ -46,28 +57,37 @@ def get_meta_data(**args):
     data_dict = {}
     # This information can be retrieved with one single Solr query
     # (just an 'OR' query of a list of bibcodes)
-    bibcodes = [bibcode for (bibcode,score) in args['results']]
-    list = " OR ".join(map(lambda a: "bibcode:%s"%a, bibcodes))
+    bibcodes = [bibcode for (bibcode, score) in args['results']]
+    list = " OR ".join(map(lambda a: "bibcode:%s" % a, bibcodes))
     q = '%s' % list
     # Get the information from Solr
-    headers = {'X-Forwarded-Authorization' : request.headers.get('Authorization')}
-    params = {'wt':'json', 'q':q, 'fl':'bibcode,title,first_author', 'rows': current_app.config.get('CITATION_HELPER_MAX_HITS')}
-    response = current_app.config.get('CITATION_HELPER_CLIENT').session.get(current_app.config.get('CITATION_HELPER_SOLRQUERY_URL'), params=params, headers=headers)
+    headers = {'X-Forwarded-Authorization':
+               request.headers.get('Authorization')}
+    params = {'wt': 'json', 'q': q, 'fl': 'bibcode,title,first_author',
+              'rows': _app.config.get('CITATION_HELPER_MAX_HITS')}
+    response = _app.config.get('CITATION_HELPER_CLIENT').session.get(
+        _app.config.get('CITATION_HELPER_SOLRQUERY_URL'), params=params,
+        headers=headers)
     if response.status_code != 200:
-        return {"Error": "There was a connection error. Please try again later", "Error Info": response.text, "Status Code": response.status_code}
+        return {"Error": "There was a connection error",
+                "Error Info": response.text,
+                "Status Code": response.status_code}
     resp = response.json()
     # Collect meta data
     for doc in resp['response']['docs']:
         title = 'NA'
-        if 'title' in doc: title = doc['title'][0]
+        if 'title' in doc:
+            title = doc['title'][0]
         author = 'NA'
-        if 'first_author' in doc: author = "%s,+"%doc['first_author'].split(',')[0]
-        data_dict[doc['bibcode']] = {'title':title, 'author':author}
+        if 'first_author' in doc:
+            author = "%s,+" % doc['first_author'].split(',')[0]
+        data_dict[doc['bibcode']] = {'title': title, 'author': author}
     return data_dict
 
+
 def chunks(l, n):
-    """ 
+    """
     Yield successive n-sized chunks from l.
     """
     for i in xrange(0, len(l), n):
-        yield l[i:i+n]
+        yield l[i:i + n]
--- service/views.py
+++ service/views.py
@@ -8,22 +8,29 @@ import sys
 from utils.citation_helper import get_suggestions
 
 blueprint = Blueprint(
-      'citation_helper',
-      __name__,
-      static_folder=None,
+    'citation_helper',
+    __name__,
+    static_folder=None,
 )
 
+
 class CitationHelper(Resource):
+
     """computes Citation Helper suggestions on the POST body"""
     scopes = []
-    rate_limit = [1000,60*60*24]
-    decorators = [advertise('scopes','rate_limit')]
+    rate_limit = [1000, 60 * 60 * 24]
+    decorators = [advertise('scopes', 'rate_limit')]
+
     def post(self):
-        if not request.json or not 'bibcodes' in request.json:
-            return {'Error': 'Unable to get results!', 'Error Info':'No bibcodes found in POST body'}, 200
+        if not request.json or 'bibcodes' not in request.json:
+            return {'Error': 'Unable to get results!',
+                    'Error Info': 'No bibcodes found in POST body'}, 200
         bibcodes = map(str, request.json['bibcodes'])
-        if len(bibcodes) > current_app.config.get('CITATION_HELPER_MAX_SUBMITTED'):
-            return {'Error':'Unable to get results!', 'Error Info': 'Number of submitted bibcodes exceeds maximum number'}, 200 
+        if len(bibcodes) > \
+                current_app.config.get('CITATION_HELPER_MAX_SUBMITTED'):
+            return {'Error': 'Unable to get results!',
+                    'Error Info':
+                    'Number of submitted bibcodes exceeds maximum number'}, 200
 
         results = get_suggestions(bibcodes=bibcodes)
         if "Error" in results:
@@ -32,4 +39,5 @@ class CitationHelper(Resource):
         if results:
             return results
         else:
-            return {'Error':'Unable to get results!', 'Error Info': 'Could not find anything to suggest'}, 200
+            return {'Error': 'Unable to get results!',
+                    'Error Info': 'Could not find anything to suggest'}, 200
