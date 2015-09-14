f89c9fa3608483f75af2afb8744fa882db550d0c
Edwin Henneken <ehenneken@cfa.harvard.edu>
2015-09-14 09:15:23 -0400

    added logging and version files

--- service/config.py
+++ service/config.py
@@ -7,6 +7,38 @@ CITATION_HELPER_NUMBER_SUGGESTIONS = 10
 CITATION_HELPER_THRESHOLD_FREQUENCY = 1
 API_URL = 'https://api.adsabs.harvard.edu'
 CITATION_HELPER_SOLRQUERY_URL = '%s/v1/search/query' % API_URL
+# Config for logging
+CITATION_HELPER_LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(levelname)s\t%(process)d '
+                      '[%(asctime)s]:\t%(message)s',
+            'datefmt': '%m/%d/%Y %H:%M:%S',
+        }
+    },
+    'handlers': {
+        'file': {
+            'formatter': 'default',
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': '/tmp/citation_helper.log',
+        },
+        'console': {
+            'formatter': 'default',
+            'level': 'INFO',
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file', 'console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
 # Define the autodiscovery endpoint
 DISCOVERER_PUBLISH_ENDPOINT = '/resources'
 # Advertise its own route within DISCOVERER_PUBLISH_ENDPOINT
--- service/views.py
+++ service/views.py
@@ -2,6 +2,7 @@ from flask import current_app, request
 from flask.ext.restful import Resource
 from flask.ext.discoverer import advertise
 from citation_helper import get_suggestions
+import time
 
 
 class CitationHelper(Resource):
@@ -12,22 +13,29 @@ class CitationHelper(Resource):
     decorators = [advertise('scopes', 'rate_limit')]
 
     def post(self):
+        stime = time.time()
         if not request.json or 'bibcodes' not in request.json:
+            current_app.logger.error('No bibcodes were provided to Citation Helper')
             return {'Error': 'Unable to get results!',
                     'Error Info': 'No bibcodes found in POST body'}, 200
         bibcodes = map(str, request.json['bibcodes'])
         if len(bibcodes) > \
                 current_app.config.get('CITATION_HELPER_MAX_SUBMITTED'):
+            current_app.logger.warning('Citation Helper called with %s bibcodes. Maximum is: %s!'%(len(bibcodes),current_app.config.get('CITATION_HELPER_MAX_SUBMITTED')))
             return {'Error': 'Unable to get results!',
                     'Error Info':
                     'Number of submitted bibcodes exceeds maximum number'}, 200
 
         results = get_suggestions(bibcodes=bibcodes)
         if "Error" in results:
+            current_app.logger.error('Citation Helper request request blew up')
             return results, 500
 
         if results:
+            duration = time.time() - stime
+            current_app.logger.info('Citation Helper request successfully completed in %s real seconds'%duration)
             return results
         else:
+            current_app.logger.info('Citation Helper request returned empty result')
             return {'Error': 'Unable to get results!',
                     'Error Info': 'Could not find anything to suggest'}, 200
